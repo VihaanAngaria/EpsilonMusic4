44ccf88fa381bf418da1f0028092e6d9c706d999
VihaanAngaria <vihaanangaria@gmail.com>
2026-08-30 01:29:13 +0530

    Add Vercel WebView URL config

---
+++ app/src/main/java/com/epsilonmusic/app/EpsilonUrlConfig.java
@@ -0,0 +1,6 @@
+package com.epsilonmusic.app;
+
+public final class EpsilonUrlConfig {
+    private EpsilonUrlConfig() {}
+    public static final String HOME_URL = "https://epsilonmusic.vercel.app";
+}
