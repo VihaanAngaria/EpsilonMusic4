2aaeb01f846405d989098503c4f902dcfe6d55be
VihaanAngaria <vihaanangaria@gmail.com>
2026-08-30 01:29:40 +0530

    Remove temporary URL config

--- app/src/main/java/com/epsilonmusic/app/EpsilonUrlConfig.java
+++
@@ -1,6 +0,0 @@
-package com.epsilonmusic.app;
-
-public final class EpsilonUrlConfig {
-    private EpsilonUrlConfig() {}
-    public static final String HOME_URL = "https://epsilonmusic.vercel.app";
-}
